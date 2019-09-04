dbdf6d5a2221b1a8d3c8e4acb2982f5f28b2d8fd
陈俊甫 <junfu.chen@outlook.com>
2019-09-04 17:49:29 +0800

    first commit

--- src/main/java/com/lazulite/rse/config/Constants.java
+++ src/main/java/com/lazulite/rse/config/Constants.java
@@ -12,6 +12,14 @@ public final class Constants {
     public static final String DEFAULT_LANGUAGE = "zh-tw";
     public static final String ANONYMOUS_USER = "anonymoususer";
 
+
+    public static final String REPEAT_LOGIN = "repeatLogin";
+
+    public static final String DEFAULT_ALIPAY_USER_PASSWORD = "123456";
+    public static final int REPEAT_LOGIN_CACHE_TIME = 3;
+
+    public static final String NOTIFY_URL = "https://oapiuat.fosun.com/rs/api/alipay/notify";
+
     private Constants() {
     }
 }
